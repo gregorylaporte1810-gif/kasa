2d0110a9809cfc937d1e0900e59471f74733d1bb
gregorylaporte1810-gif <gregorylaporte1810@gmail.com>
2026-09-04 09:07:42 +0200

    feat: add 404 error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+import "./Error.scss";
+
 function Error() {
   return (
-    <main>
-      <h1>404</h1>
-      <p>La page que vous demandez n'existe pas.</p>
-    </main>
+    <section className="error-page">
+      <h1 className="error-page__code">404</h1>
+
+      <p className="error-page__message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+
+      <Link className="error-page__link" to="/">
+        Retourner sur la page d’accueil
+      </Link>
+    </section>
   );
 }
 
